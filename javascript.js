39984af8bb3773b0ed4c2eab60300ea1663d92fd
Avnish Yadav <112178009+Avnishx@users.noreply.github.com>
2023-02-12 21:38:12 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,26 @@
+let btn = document.getElementById("btn");
+let qrImg = document.getElementById("qrImg");
+let qrText = document.getElementById("qrText");
+let imgBox = document.getElementById("imgBox");
+
+
+btn.onclick = function (){
+ 
+    if(qrText.value.length > 0){
+
+    qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+
+    imgBox.classList.add("show-img");
+    
+    }
+    else{
+       qrText.classList.add("error");
+       setTimeout(()=> {
+           qrText.classList.remove("error")
+       },1000)
+
+
+    }
+
+
+}
